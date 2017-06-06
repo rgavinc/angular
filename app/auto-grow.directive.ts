a8afd6fac98205fe5a5932f4b2e6d05743c79bba
Gavin Robertson <gavin.robertson@byu.edu>
2017-06-06 09:33:59 -0600

    First commit

---
+++ app/auto-grow.directive.ts
@@ -0,0 +1,28 @@
+import {Directive, ElementRef, Renderer} from 'angular2/core'
+
+@Directive({
+    selector: '[autoGrow]',
+    host: {
+        '(focus)': 'onFocus()',
+        '(blur)': 'onBlur()'
+    }
+})
+
+export class AutoGrowDirective {
+    _el: ElementRef;
+    constructor(private el: ElementRef, private renderer: Renderer){
+        this.renderer.setElementStyle(this.el.nativeElement, 'background-color', 'red');
+
+    }
+
+    onFocus(){
+        this.renderer.setElementStyle(this.el.nativeElement, 'width', '200px');
+        this.renderer.setElementStyle(this.el.nativeElement, 'transition', 'all 0.5s');
+        
+    }
+
+    onBlur(){
+        this.renderer.setElementStyle(this.el.nativeElement, 'width', '120px');
+        this.renderer.setElementStyle(this.el.nativeElement, 'transition', 'all 0.5s');
+    }
+}
